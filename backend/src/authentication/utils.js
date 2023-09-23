8d134db28976e2d07a04a19218c1013d1ee4f2bc
ltrinks <laurentrinks@gmail.com>
2023-09-23 18:00:49 -0500

    login logout

--- backend/src/authentication/utils.js
+++ backend/src/authentication/utils.js
@@ -1,4 +1,5 @@
 import { sha256 } from 'js-sha256';
+import { User } from './schemas.js';
 
 /* 
 Takes a username, a password, and a callback function
@@ -7,8 +8,20 @@ if error: return cb(err)
 if password fail: return cb(null, false)
 if password matches: return cb(null, user object) 
 */
-export function verify(username, password, cb) {
-    
+export async function verify(username, password, cb) {
+    // if username not found, verify fail
+    const users = await User.find({username: username});
+    if (users.length != 1) {
+        return cb(null, false);
+    }
+    const user = users[0]
+
+    // if password matches, give user, otherwise fail
+    if (user.password_hash == hash(password)) {
+        return cb(null, user)
+    } else {
+        return cb(null, false)
+    } 
 }
 
 export function hash(content) {
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,12 @@ import body_parser from 'body-parser';
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
 import mongoose from 'mongoose';
+import MongoStore from 'connect-mongo';
+
 
 import { verify, hash } from './authentication/utils.js';
 import { User } from './authentication/schemas.js';
-import { createUser } from "./authentication/endpoints.js";
+import { createUser, login, logout, auth_test } from "./authentication/endpoints.js";
 
 const MONGO_STAGE = process.env.MONGO_STAGE
 const MONGO_URL = process.env.MONGO_URL
@@ -18,44 +20,43 @@ const app = express();
 app.use(body_parser.json())
 app.use(body_parser.urlencoded({ extended: false }));
 
-// // set up sessions and add them to the app
-// app.use(session({
-//     secret: 'keyboard cat',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: { secure: true }
-//   }));
-
-// // set up passport
-// passport.use(new LocalStrategy(verify));
-
-// passport.serializeUser(function(user, cb) {
-//     process.nextTick(function() {
-//         return cb(null, {
-//         id: user.id,
-//         username: user.username,
-//         picture: user.picture
-//     });
-// });
-// });
-    
-// passport.deserializeUser(function(user, cb) {
-//     process.nextTick(function() {
-//         return cb(null, user);
-//     });
-// });
-
-// assign endpoints to the app
-app.get('/test', (req, res, next) => {
-    res.send("test")
-})
-
-app.post("/create_user", createUser)
-
 // connect to mongodb
 let connection_url = `mongodb://${MONGO_URL}:27017/${MONGO_STAGE}`;
 mongoose.set('strictQuery', false);
 await mongoose.connect(connection_url);
+const db = mongoose.connection;
+
+// set up sessions and add them to the app
+app.use(session({
+    secret: 'hubit secret',
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongoUrl: db.client.s.url })
+}));
+
+// set up passport
+passport.use(new LocalStrategy(verify));
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+        return cb(null, user);
+    });
+});
+    
+passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+        return cb(null, user);
+    });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// auth endpoints
+app.post('/login', login);
+app.post('/logout', logout);
+app.post('/create_user', createUser)
+app.get('/auth_test', auth_test)
 
 // start the app
 const server = app.listen(3000, function () {
