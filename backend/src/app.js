24fc3a214fcb4055e1dc8ba884c8bfadd9c4e616
ltrinks <laurentrinks@gmail.com>
2023-09-29 11:46:18 -0400

    prettier changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,6 @@ import body_parser from 'body-parser';
 import useAuthentication from './authentication/index.js';
 
 export default async function createApp() {
-
   // set up express and define app
   let app = express();
   app.use(body_parser.json());
