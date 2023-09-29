72b900adce217e80a9c65475ecf5c34028fb7e49
ltrinks <laurentrinks@gmail.com>
2023-09-29 00:36:44 -0400

    move more into auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,48 +1,23 @@
 import express from 'express';
-import session from 'express-session';
 import body_parser from 'body-parser';
-import passport from 'passport';
-import LocalStrategy from 'passport-local';
 import mongoose from 'mongoose';
-import MongoStore from 'connect-mongo';
 
-import { verify, serializeUser, deserializeUser } from './authentication/utils.js';
-import { createUser, login, logout, auth_test } from "./authentication/endpoints.js";
+import use_authentication from './authentication/index.js';
 
 export default async function createApp() {
     const MONGO_URL = process.env.MONGO_URL
 
     // set up express and define app
-    const app = express();
-
+    let app = express();
     app.use(body_parser.json())
     app.use(body_parser.urlencoded({ extended: false }));
 
     // connect to mongodb
     mongoose.set('strictQuery', false);
     await mongoose.connect(MONGO_URL);
-    const db = mongoose.connection;
 
-    // set up sessions and add them to the app
-    app.use(session({
-        secret: 'hubit secret',
-        resave: false,
-        saveUninitialized: true,
-        store: new MongoStore({ mongoUrl: db.client.s.url })
-    }));
-
-    // set up passport
-    passport.use(new LocalStrategy(verify));
-    passport.serializeUser(serializeUser);
-    passport.deserializeUser(deserializeUser);
-    app.use(passport.initialize());
-    app.use(passport.session());
-
-    // auth endpoints
-    app.post('/login', login);
-    app.delete('/logout', logout);
-    app.post('/create_user', createUser)
-    app.get('/auth_test', auth_test)
+    // use authentication
+    app = use_authentication(app)
 
     return app
 }
---
+++ backend/src/authentication/index.js
@@ -0,0 +1,33 @@
+import session from 'express-session';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import MongoStore from 'connect-mongo';
+import mongoose from 'mongoose';
+
+import { verify, serializeUser, deserializeUser } from './utils.js';
+import { createUser, login, logout, auth_test } from "./endpoints.js";
+
+export default function use_authentication(app) {
+    // set up sessions and add them to the app
+    app.use(session({
+        secret: 'hubit secret',
+        resave: false,
+        saveUninitialized: true,
+        store: new MongoStore({ mongoUrl: mongoose.connection.client.s.url })
+    }));
+
+    // set up passport
+    passport.use(new LocalStrategy(verify));
+    passport.serializeUser(serializeUser);
+    passport.deserializeUser(deserializeUser);
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // auth endpoints
+    app.post('/login', login);
+    app.delete('/logout', logout);
+    app.post('/create_user', createUser)
+    app.get('/auth_test', auth_test)
+
+    return app
+}
