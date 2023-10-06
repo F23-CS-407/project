341dd16b406de890083d0b3591c8179d7721bc58
vandana-c <chariv@purdue.edu>
2023-10-06 12:09:08 -0400

    Search bar call to backend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,12 +10,14 @@ export class AppComponent {
   currently_editting: boolean = false;
   searchCriteria: string = ''; // Search in sidebar
   sidePanelVisible: boolean = false; // Initialize as hidden
+  searchResults: any[] = [];
   
     performSearch() { 
       if (this.searchCriteria.trim() != '') {
         console.log('Perform search with criteria: ', this.searchCriteria);
         // TODO: Make HTTP request to backend
-  
+        // Example: Make a GET request to the backend search endpoint
+        
       } else {
         console.log('Invalid search criteria:', this.searchCriteria);
       }
