b22e050cdc30e6b0035faf92d8a2299b3b6d0fc0
vandana-c <chariv@purdue.edu>
2023-10-06 12:48:27 -0400

    Search bar call

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { HttpBackend } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,17 +11,28 @@ export class AppComponent {
   title = 'frontend';
   currently_editting: boolean = false;
   searchCriteria: string = ''; // Search in sidebar
+  searchResults: any[] = []; // Array to store search results
   sidePanelVisible: boolean = false; // Initialize as hidden
-  searchResults: any[] = [];
+
+constructor(private http: HttpClient) { }
   
-    performSearch() { 
-      if (this.searchCriteria.trim() != '') {
-        console.log('Perform search with criteria: ', this.searchCriteria);
-        // TODO: Make HTTP request to backend
-        // Example: Make a GET request to the backend search endpoint
+performSearch() { 
+  if (this.searchCriteria.trim() != '') {
+    console.log('Perform search with criteria: ', this.searchCriteria);
+
+     const api = 'http://localhost:8080/api';
+
+      this.http.get(api, { params: { query: this.searchCriteria } }).subscribe(
+      (response: any) => {
+        // Handle the response from the backend
+        this.searchResults = response;
+      },
+      // TODO: Make HTTP request to backend
+    )
         
+  
       } else {
-        console.log('Invalid search criteria:', this.searchCriteria);
+        console.error('Invalid search criteria:', this.searchCriteria);
       }
       
     } 
