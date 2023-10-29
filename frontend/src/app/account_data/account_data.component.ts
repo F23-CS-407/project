84e919df9ca9e1a7cc6832abd7303b6d3f69d7b8
vandana-c <chariv@purdue.edu>
2023-10-29 17:37:31 -0400

    added the fields for the account data page

--- frontend/src/app/account_data/account_data.component.ts
+++ frontend/src/app/account_data/account_data.component.ts
@@ -10,64 +10,38 @@ import { Community } from "../../models/Community";
 
 @Component({
   selector: 'app-account-data',
-  templateUrl: './accountdata.component.html',
-  styleUrls: ['./accountdata.component.css']
+  templateUrl: './account_data.component.html',
+  styleUrls: ['./account_data.component.css']
 })
 export class AccountDataComponent {
-    user: any;
-    isNameEditing: boolean;
-    isEmailEditing: boolean;
-    isProfilePictureEditing: boolean;
-    updatedName: string;
-    updatedEmail: string;
-    updatedProfilePicture: string;
-  
-    constructor() {
-      this.user = { name: 'John Doe', email: 'john@example.com', profilePicture: 'profile.jpg' };
-      this.isNameEditing = false;
-      this.isEmailEditing = false;
-      this.isProfilePictureEditing = false;
-      this.updatedName = '';
-      this.updatedEmail = '';
-      this.updatedProfilePicture = '';
+  user = {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    profilePicture: 'profile.jpg'
+  };
+
+  editingName = false;
+  editingEmail = false;
+  editingProfilePicture = false;
+
+  toggleEditing(field: string) {
+    if (field === 'name') {
+      this.editingName = !this.editingName;
+    } else if (field === 'email') {
+      this.editingEmail = !this.editingEmail;
+    } else if (field === 'profilePicture') {
+      this.editingProfilePicture = !this.editingProfilePicture;
     }
-  
-    editName() {
-        this.isNameEditing = true;
-        this.updatedName = this.user.name;
-      }
-    
-      // Add saveName function to handle saving the name
-      saveName() {
-        this.user.name = this.updatedName; // Update the name with the edited value
-        this.isNameEditing = false; // Exit edit mode
-        // Send an HTTP request to update the user's name on the backend
-      }
-    
-      editEmail() {
-        this.isEmailEditing = true;
-        this.updatedEmail = this.user.email;
-      }
-    
-      // Add saveEmail function to handle saving the email
-      saveEmail() {
-        this.user.email = this.updatedEmail; // Update the email with the edited value
-        this.isEmailEditing = false; // Exit edit mode
-        // Send an HTTP request to update the user's email on the backend
-      }
-    
-      editProfilePicture() {
-        this.isProfilePictureEditing = true;
-        this.updatedProfilePicture = this.user.profilePicture;
-      }
-    
-      // Add saveProfilePicture function to handle saving the profile picture
-      saveProfilePicture() {
-        this.user.profilePicture = this.updatedProfilePicture; // Update the profile picture with the edited value
-        this.isProfilePictureEditing = false; // Exit edit mode
-        // Send an HTTP request to update the user's profile picture on the backend
-      }
-    
-    // ... other code
   }
-  
+
+  updateProfilePicture(event: any) {
+    const file = event.target.files[0];
+    // Handle the file upload and update the user's profile picture
+    // Set the user's profile picture URL here.
+  }
+
+  saveChanges() {
+    // Send an HTTP request to update the user's account data on the backend based on the changes made to this.user.
+    
+  }
+}
