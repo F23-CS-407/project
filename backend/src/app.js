a55fe4c2da51baa6c2c67ced604b9ed03b1c260d
ltrinks <laurentrinks@gmail.com>
2023-11-01 02:55:13 -0400

    basic example

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import body_parser from 'body-parser';
+import multer from 'multer';
+import axios from 'axios';
+import fs from 'fs';
+import FormData from 'form-data';
 
 import useAuthentication from './authentication/index.js';
 import useCommunities from './communities/index.js';
@@ -19,6 +23,62 @@ export default async function createApp(options) {
 
   // use users
   app = useUser(app);
+
+  let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/usr/backend/uploads');
+    },
+    filename: function (req, file, cb) {
+      let extArray = file.mimetype.split('/');
+      let extension = extArray[extArray.length - 1];
+      cb(null, file.fieldname + '-' + Date.now() + '.' + extension);
+    },
+  });
+
+  const upload = multer({ storage: storage });
+
+  app.post('/upload', upload.single('file'), function (req, res) {
+    const title = req.body.title;
+    const file = req.file;
+
+    console.log(title);
+    console.log(file);
+
+    res.send(file.filename);
+  });
+
+  app.get('/uploader', (req, res) => {
+    const uploader = async () => {
+      try {
+        const file = fs.createReadStream('./mo.jpeg');
+        const title = 'My file';
+
+        const form = new FormData();
+        form.append('title', title);
+        form.append('file', file);
+
+        const resp = await axios.post('http://localhost:3000/upload', form, {
+          headers: {
+            ...form.getHeaders(),
+          },
+        });
+
+        if (resp.status === 200) {
+          return resp.data;
+        }
+      } catch (err) {
+        return new Error(err.message);
+      }
+    };
+    uploader().then((resp) => res.send(resp));
+    return;
+  });
+
+  app.get('/upload', (req, res) => {
+    const name = req.query.name;
+
+    res.sendFile(`/usr/backend/uploads/${name}`);
+  });
 
   return app;
 }
