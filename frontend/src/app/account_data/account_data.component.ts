5ee04b4d7ce2ae2bb6d05091b6dee08b34f159a5
vandana-c <chariv@purdue.edu>
2023-11-01 16:00:45 -0400

    fixed endpoints

--- frontend/src/app/account_data/account_data.component.ts
+++ frontend/src/app/account_data/account_data.component.ts
@@ -1,26 +1,18 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { Clipboard } from '@angular/cdk/clipboard';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import { User } from "../../models/User";
-import { Alias } from "../../models/Alias";
-import { Post } from "../../models/Post";
-import { Community } from "../../models/Community";
 
 @Component({
   selector: 'app-account-data',
   templateUrl: './account_data.component.html',
   styleUrls: ['./account_data.component.css']
 })
-export class AccountDataComponent {
+export class AccountDataComponent implements OnInit {
   private backend_addr: string = "http://localhost:8080/api";
 
   user = {
-    name: 'John Doe',
-    password: '',
-    description: 'This is my profile description',
-    profilePicture: 'profile.jpg'
+    name: '', // Initialize as empty string
+    password: '', // Initialize as empty string
+    description: '', // Initialize as empty string
   };
 
   editingName = false;
@@ -29,10 +21,30 @@ export class AccountDataComponent {
 
   constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    // Fetch user data from the backend during component initialization
+    this.fetchUserData();
+  }
+
+  fetchUserData() {
+    const options = { withCredentials: true };
+    this.http.get<any>(this.backend_addr + "/user_info", options).subscribe({
+      next: userData => {
+        // Populate the user object with the fetched data
+        this.user.name = userData.username;
+        this.user.description = userData.description;
+        // Password should not be fetched
+      },
+      error: error => {
+        console.error('Failed to fetch user data:', error);
+      }
+    });
+  }
+
   toggleEditing(field: string) {
     if (field === 'name') {
       this.editingName = !this.editingName;
-    } else if (field === 'email') {
+    } else if (field === 'password') {
       this.editingPassword = !this.editingPassword;
     } else if (field === 'description') {
       this.editingDescription = !this.editingDescription;
@@ -40,32 +52,28 @@ export class AccountDataComponent {
   }
 
   saveChanges() {
+    const options = { withCredentials: true };
     if (this.editingName) {
       // Send a request to update the name
-      const options = { withCredentials: true };
-      const body = { new_name: this.user.name };
-      
-      this.http.post<any>(this.backend_addr + "/change_name", body, options).subscribe({
+      const body = { new_username: this.user.name };
+      this.http.post<any>(this.backend_addr + "/change_username", body, options).subscribe({
         next: response => {
           console.log('Name updated successfully:', response);
           this.editingName = false; // Disable editing mode after success
         },
         error: error => {
-          console.error('Failed to update name:', error);
-          // Handle the error, e.g., show an error message
+          console.error('Failed to update name:', error) // Show an error message
         }
       });
     }
 
-    if (this.editingPassword) { // Change 'email' to 'password'
+    if (this.editingPassword) { // Password
       // Send a request to update the password
-      const options = { withCredentials: true };
-      const body = { new_password: this.user.password }; // Change 'email' to 'password'
-      
+      const body = { new_password: this.user.password };
       this.http.post<any>(this.backend_addr + "/change_password", body, options).subscribe({
         next: response => {
           console.log('Password updated successfully:', response);
-          this.editingPassword = false; // Change editingEmail to editingPassword
+          this.editingPassword = false; // Disable editing mode after success
         },
         error: error => {
           console.error('Failed to update password:', error);
@@ -74,18 +82,15 @@ export class AccountDataComponent {
     }
 
     if (this.editingDescription) {
-      // Send a request to update the description
-      const options = { withCredentials: true };
+      // Send request to update the description
       const body = { new_description: this.user.description };
-      
       this.http.post<any>(this.backend_addr + "/change_description", body, options).subscribe({
         next: response => {
           console.log('Description updated successfully:', response);
           this.editingDescription = false; // Disable editing mode after success
         },
         error: error => {
-          console.error('Failed to update description:', error);
-          // Handle the error, e.g., show an error message
+          console.error('Failed to update description:', error); // Display error message 
         }
       });
     }
