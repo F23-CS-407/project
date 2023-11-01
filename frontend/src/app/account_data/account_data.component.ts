ce5f435a7d9defb3d1210d93875829f368b63be1
vandana-c <chariv@purdue.edu>
2023-11-01 15:08:28 -0400

    added backend endpoints

--- frontend/src/app/account_data/account_data.component.ts
+++ frontend/src/app/account_data/account_data.component.ts
@@ -14,17 +14,18 @@ import { Community } from "../../models/Community";
   styleUrls: ['./account_data.component.css']
 })
 export class AccountDataComponent {
+  private backend_addr: string = "http://localhost:8080/api";
+
   user = {
     name: 'John Doe',
-    email: 'johndoe@example.com',
     password: '',
+    description: 'This is my profile description',
     profilePicture: 'profile.jpg'
   };
 
   editingName = false;
-  editingEmail = false;
   editingPassword = false;
-  editingProfilePicture = false;
+  editingDescription = false;
 
   constructor(private http: HttpClient) {}
 
@@ -32,42 +33,61 @@ export class AccountDataComponent {
     if (field === 'name') {
       this.editingName = !this.editingName;
     } else if (field === 'email') {
-      this.editingEmail = !this.editingEmail;
-    } else if (field === 'password') { 
       this.editingPassword = !this.editingPassword;
-    } else if (field === 'profilePicture') {
-      this.editingProfilePicture = !this.editingProfilePicture;
+    } else if (field === 'description') {
+      this.editingDescription = !this.editingDescription;
     }
-  }
-
-  updateProfilePicture(event: any) {
-    const file = event.target.files[0];
-    // Handle the file upload and update the user's profile picture
-    if(file) {
-      console.log('Selected file:', file);
-    }
-    // Set the user's profile picture URL here.
   }
 
   saveChanges() {
-    const updateUser = {
-      name: this.user.name,
-      email: this.user.email,
-      password: this.user.password,
-      // Add other properties as needed
-    };
+    if (this.editingName) {
+      // Send a request to update the name
+      const options = { withCredentials: true };
+      const body = { new_name: this.user.name };
+      
+      this.http.post<any>(this.backend_addr + "/change_name", body, options).subscribe({
+        next: response => {
+          console.log('Name updated successfully:', response);
+          this.editingName = false; // Disable editing mode after success
+        },
+        error: error => {
+          console.error('Failed to update name:', error);
+          // Handle the error, e.g., show an error message
+        }
+      });
+    }
 
-    // Send an HTTP request to update the user's account data on the backend based on the changes made to this.user.
-    this.http.post<any>('/api/updateUser', updateUser).subscribe({
-      next: (response) => {
-        console.log('User data updated successfully:', response);
-        // Handle the success response, e.g., show a success message.
-      },
-      error: (error) => {
-        console.error('Failed to update user data:', error);
-        // Handle the error, e.g., show an error message.
-      }
-    });
-    
+    if (this.editingPassword) { // Change 'email' to 'password'
+      // Send a request to update the password
+      const options = { withCredentials: true };
+      const body = { new_password: this.user.password }; // Change 'email' to 'password'
+      
+      this.http.post<any>(this.backend_addr + "/change_password", body, options).subscribe({
+        next: response => {
+          console.log('Password updated successfully:', response);
+          this.editingPassword = false; // Change editingEmail to editingPassword
+        },
+        error: error => {
+          console.error('Failed to update password:', error);
+        }
+      });
+    }
+
+    if (this.editingDescription) {
+      // Send a request to update the description
+      const options = { withCredentials: true };
+      const body = { new_description: this.user.description };
+      
+      this.http.post<any>(this.backend_addr + "/change_description", body, options).subscribe({
+        next: response => {
+          console.log('Description updated successfully:', response);
+          this.editingDescription = false; // Disable editing mode after success
+        },
+        error: error => {
+          console.error('Failed to update description:', error);
+          // Handle the error, e.g., show an error message
+        }
+      });
+    }
   }
 }
