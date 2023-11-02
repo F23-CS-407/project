93367f618f2eeca65bfc53b3f3546e7b6e1e4735
vandana-c <chariv@purdue.edu>
2023-11-02 13:15:12 -0400

    stylistic changes to search results

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -15,31 +15,37 @@ export class AppComponent {
 
 constructor(private http: HttpClient) { }
   
-performSearch() { 
-  if (this.searchCriteria.trim() != '') {
-    console.log('Perform search with criteria: ', this.searchCriteria);
+performSearch() {
+    if (this.searchCriteria.trim() !== '') {
+      console.log('Perform search with criteria: ', this.searchCriteria);
 
-     const api = 'http://localhost:8080/api';
+      const api = 'http://localhost:8080/api';
 
-      this.http.get<any>(api + `/search_users?username=${this.searchCriteria}`).subscribe(
-      (response: any) => {
-        // Handle the response from the backend
-        this.searchResults = response;
-        console.log(response);
-      },
-      
-    );
-        
-  
-      } else {
-        console.error('Invalid search criteria:', this.searchCriteria);
-      }
-      
-    } 
-  
-    toggleSidePanel() {
-      this.sidePanelVisible = !this.sidePanelVisible;
+      // Perform both user and community searches
+      this.http
+        .get<any>(api + `/search_users?username=${this.searchCriteria}`)
+        .subscribe(
+          (userResponse: any) => {
+            this.searchResults = userResponse;
+            console.log('User search results:', userResponse);
+          }
+        );
+
+      this.http
+        .get<any>(api + `/search_communities?communityName=${this.searchCriteria}`)
+        .subscribe(
+          (communityResponse: any) => {
+            // Merge the community results with user results
+            this.searchResults = this.searchResults.concat(communityResponse);
+            console.log('Community search results:', communityResponse);
+          }
+        );
+    } else {
+      console.error('Invalid search criteria:', this.searchCriteria);
     }
+  }
 
-
+  toggleSidePanel() {
+    this.sidePanelVisible = !this.sidePanelVisible;
+  }
 }
