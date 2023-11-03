6fa71ed9c42e898ea5fdaf8561733f91a61cfc5a
Ramon del Castillo <rdelcastillo@devnext.net>
2023-11-02 20:06:35 -0400

    menu and community home update

--- frontend/src/app/features/hubit/community/community.component.ts
+++ frontend/src/app/features/hubit/community/community.component.ts
@@ -24,63 +24,68 @@ export class CommunityComponent {
   constructor(private http: HttpClient, private router: Router) {
     // Get community query
     this.community_id = this.urlParams.get('community') as string;
-    if (this.community_id == undefined || this.community_id == ""){
-      // Invalid community id
-      this.router.navigate(["/"]);
-    }
+    // if (this.community_id == undefined || this.community_id == ""){
+    //   // Invalid community id
+    //   this.router.navigate(["/"]);
+    // }
   
-    // Get community data
-    const options = { withCredentials : true };
-    this.http.get<any>(this.backend_addr + "/community?id="+this.community_id, options).subscribe({
-      next: get_community_response => {          // On success
-        this.community_name = get_community_response.name;
-        this.community_desc = get_community_response.description;
+    // // Get community data
+    // const options = { withCredentials : true };
+    // this.http.get<any>(this.backend_addr + "/community?id="+this.community_id, options).subscribe({
+    //   next: get_community_response => {          // On success
+    //     this.community_name = get_community_response.name;
+    //     this.community_desc = get_community_response.description;
         
-        // TODO: When community modification is added, check if logged in user is a mod with "response.mods"
-      }, 
-      error: error => {         // On fail
-        console.log(error);
+    //     // TODO: When community modification is added, check if logged in user is a mod with "response.mods"
+    //   }, 
+    //   error: error => {         // On fail
+    //     console.log(error);
 
-        // Trying to view a community that doesn't exist
-        this.router.navigate(["/"]);
-      }});
+    //     // Trying to view a community that doesn't exist
+    //     this.router.navigate(["/"]);
+    //   }});
       
-    // Get Community Posts Data
-    this.http.get<any>(this.backend_addr + "/community/posts?community="+this.community_id, options).subscribe({
-      next: get_community_posts_response => {          // On success
-        for (let i = 0; i < get_community_posts_response.length; i++) {
-          // Get post as object
-          let curr_post = get_community_posts_response[i];
+    // // Get Community Posts Data
+    // this.http.get<any>(this.backend_addr + "/community/posts?community="+this.community_id, options).subscribe({
+    //   next: get_community_posts_response => {          // On success
+    //     for (let i = 0; i < get_community_posts_response.length; i++) {
+    //       // Get post as object
+    //       let curr_post = get_community_posts_response[i];
 
-          // Get username from user_id
-          let post_username: string = this.get_username(curr_post.created_by);
+    //       // Get username from user_id
+    //       let post_username: string = await this.get_username(curr_post.created_by).catch((error) => {
+    //         console.error(error);
+    //         return "Username not found";
+    //       });
 
-          // Create post object
-          let new_post: Post = new Post(post_username);
-          new_post.content = curr_post.content;
+    //       // Create post object
+    //       let new_post: Post = new Post(post_username);
+    //       new_post.content = curr_post.content;
           
-          // Add post to list for display
-          this.community_posts.push(new_post);
-        }
-      }, 
-      error: error => {         // On fail
-        console.log(error);
-      }});
+    //       // Add post to list for display
+    //       this.community_posts.push(new_post);
+    //     }
+    //   }, 
+    //   error: error => {         // On fail
+    //     console.log(error);
+    //   }});
   }
 
   /** 
    Given user id, returns associated account username
    **/
-  private get_username(user_id: string) {
-    const options = { withCredentials : true };
-    this.http.get<any>(this.backend_addr + "/user?id="+user_id, options).subscribe({
-      next: get_user_response => {          // On success
-        return get_user_response.username;
-      }, 
-      error: error => {         // On fail
-        
-      }});
-
-    return "get_username() is broken";
-  }
+   private get_username(user_id: string): Promise<string> {
+    const options = { withCredentials: true };
+    return new Promise((resolve, reject) => {
+      this.http.get<any>(`${this.backend_addr}/user?id=${user_id}`, options).subscribe({
+        next: get_user_response => {
+          resolve(get_user_response.username);
+        },
+        error: error => {
+          console.error(error);
+          reject("Unable to get username");
+        }
+      });
+    });
+  }  
 }
