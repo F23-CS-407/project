3fa39dea7333c652cf0767aae036f9ac283c9b9b
ltrinks <laurentrinks@gmail.com>
2023-11-03 03:48:04 -0400

    create board button only if mod

--- frontend/src/app/boards/boards.component.ts
+++ frontend/src/app/boards/boards.component.ts
@@ -11,7 +11,11 @@ import { NewBoardComponent } from '../new-board/new-board.component';
 })
 export class BoardsComponent implements OnInit {
   community_id: string = '';
-  boards: any[] = []; 
+  boards: any[] = [];
+  logged_in = false;
+  self_id = "";
+  community: any = {}
+  is_mod = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -29,7 +33,6 @@ export class BoardsComponent implements OnInit {
     }
   }
 
-
   openCreateBoardDialog(): void {
     const dialogRef = this.dialog.open(NewBoardComponent, {
       width: '250px',
@@ -43,6 +46,20 @@ export class BoardsComponent implements OnInit {
     });
   }
 
+  getCommunity() {
+    this.http.get<any[]>(`/api/community?id=${this.community_id}`).subscribe({
+      next: (data) => {
+        this.community = data;
+        if (this.community.mods.includes(this.self_id)) {
+          this.is_mod = true;
+        }
+      },
+      error: (error) => {
+        console.error('There was an error fetching the boards', error);
+      }
+    });
+  }
+
   getBoards(): void {
     this.http.get<any[]>(`/api/community/boards?id=${this.community_id}`).subscribe({
       next: (data) => {
@@ -52,5 +69,20 @@ export class BoardsComponent implements OnInit {
         console.error('There was an error fetching the boards', error);
       }
     });
+  }
+
+  getData() {
+    const options = { withCredentials : true};
+    this.http.get<any>("/api/user_info", options).subscribe({
+      next: info_response => {          // On success
+        this.logged_in = true;
+        this.self_id = info_response._id;
+        console.log(info_response);
+        this.getCommunity()
+      }, 
+      error: error => {         // On fail
+        console.log("No session: ");
+        console.log(error);
+      }});
   }
 }
