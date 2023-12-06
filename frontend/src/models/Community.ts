69875416b036b0d97378979dfc5716b107738f82
Ramon del Castillo <rdelcastillo@devnext.net>
2023-12-05 22:13:38 -0500

    updated models

--- frontend/src/models/Community.ts
+++ frontend/src/models/Community.ts
@@ -1,4 +1,7 @@
 import { User } from './User';
 import { Board } from './Board';
 
-export class Community {}
+export class Community {
+    public name: string = "";
+    public id: string = "";
+}
