e4b0e57359617866e10690cd62546aa3443a1a03
ltrinks <laurentrinks@gmail.com>
2023-12-08 08:46:02 -0500

    set map endpoint

--- backend/src/uploads/endpoints.js
+++ backend/src/uploads/endpoints.js
@@ -332,3 +332,61 @@ export async function uploadClip(req, res) {
       .run();
   });
 }
+
+export async function setCommunityMap(req, res) {
+  // must be a request type that can send files
+  if (!req.headers['content-type'] || !req.headers['content-type'].includes('multipart/form-data')) {
+    res.status(400).send({ error: 'content-type must be multipart/form-data' });
+    return;
+  }
+
+  // must be logged in
+  if (req.isAuthenticated()) {
+    // community must be valid and user must be mod
+    let community = req.query.id;
+    if (!mongoose.Types.ObjectId.isValid(community)) {
+      res.status(400).send({ error: 'invalid community' });
+      return;
+    }
+    community = await Community.findById(community);
+    if (community && community.mods.includes(req.user._id)) {
+      // save the file
+      upload.single('file')(req, res, async (err) => {
+        // file couldn't be fetched from request
+        if (err) {
+          res.status(400).send({ error: 'file must be sent in "file" key of form-data' });
+          return;
+        }
+
+        // get the file info and generate a receipt
+        const file = req.file;
+        let receipt = new UploadReceipt({
+          creator: req.user._id,
+          filename: file.filename,
+          url: uploadBaseUrl + file.filename,
+        });
+        receipt = await receipt.save();
+
+        let user = await User.findById(req.user._id);
+
+        // put receipt in user profile and community banner
+        user.uploads.push(receipt._id);
+        community.map = receipt.url;
+
+        await user.save();
+        await community.save();
+
+        // send new community object
+        res.send(await Community.findById(community._id));
+
+        return;
+      });
+      return;
+    } else {
+      res.status(403).send({ error: 'not mod in community' });
+      return;
+    }
+  }
+  res.status(401).send({ error: 'not logged in' });
+  return;
+}
